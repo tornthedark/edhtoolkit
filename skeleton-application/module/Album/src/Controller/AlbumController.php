0cfe50b9f64b70252f57f5b33adc65b87066fe61
tornthedark <kimbach@hotmail.com>
2018-02-19 22:51:21 +0100

    Added an Album Form

--- skeleton-application/module/Album/src/Controller/AlbumController.php
+++ skeleton-application/module/Album/src/Controller/AlbumController.php
@@ -3,8 +3,10 @@
 namespace Album\Controller;
 
 use Album\Model\AlbumTable;
+use Album\Form\AlbumForm;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+
 
 class AlbumController extends AbstractActionController
 {
@@ -24,12 +26,67 @@ class AlbumController extends AbstractActionController
 
     public function addAction()
     {
+        $form = new AlbumForm();
+        $form->get('submit')->setValue('Add');
 
+        $request = $this->getRequest();
+
+        if (! $request->isPost()) {
+            return ['form' => $form];
+        }
+
+        $album = new Album();
+        $form->setInputFilter($album->getInputFilter());
+        $form->setData($request->getPost());
+
+        if (! $form->isValid()) {
+            return ['form' => $form];
+        }
+
+        $album->exchangeArray($form->getData());
+        $this->table->saveAlbum($album);
+        return $this->redirect()->toRoute('album');
     }
 
     public function editAction()
     {
+        $id = (int) $this->params()->fromRoute('id', 0);
 
+        if (0 === $id) {
+            return $this->redirect()->toRoute('album', ['action' => 'add']);
+        }
+
+        // Retrieve the album with the specified id. Doing so raises
+        // an exception if the album is not found, which should result
+        // in redirecting to the landing page.
+        try {
+            $album = $this->table->getAlbum($id);
+        } catch (\Exception $e) {
+            return $this->redirect()->toRoute('album', ['action' => 'index']);
+        }
+
+        $form = new AlbumForm();
+        $form->bind($album);
+        $form->get('submit')->setAttribute('value', 'Edit');
+
+        $request = $this->getRequest();
+        $viewData = ['id' => $id, 'form' => $form];
+
+        if (! $request->isPost()) {
+            return $viewData;
+        }
+
+        $form->setInputFilter($album->getInputFilter());
+        $form->setData($request->getPost());
+
+        if (! $form->isValid()) {
+            return $viewData;
+        }
+
+        $this->table->saveAlbum($album);
+
+        // Redirect to album list
+        return $this->redirect()->toRoute('album', ['action' => 'index']);
     }
 
     public function deleteAction()
